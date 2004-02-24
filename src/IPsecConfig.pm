081f6a02e932151651b6d1eb675bacedafba0bf9
Ludwig Nussel <lnussel@suse.de>
2004-02-24 11:42:13 +0000

    adapt to ipsec.conf parser
    svn path=/trunk/ipsec/; revision=14657

--- src/IPsecConfig.pm
+++ src/IPsecConfig.pm
@@ -20,11 +20,15 @@ use YaST::YCP qw(Boolean);
 use Locale::gettext;
 use POSIX;     # Needed for setlocale()
 
+use lib "/usr/share/YaST2/modules"; #### FIXME!!!
+use FreeSwanUtils;
+
 YaST::YCP::Import ("SCR");
 
 setlocale(LC_MESSAGES, "");
 textdomain("ipsec");
 
+my $fsutil;
 my %connections;
 my %settings;
 
@@ -36,7 +40,8 @@ our %TYPEINFO;
 
 BEGIN
 {
-    print STDERR "constructor\n";
+    $fsutil = new FreeSwanUtils();
+    print STDERR "new FreeSwanUtils() => ", $fsutil ? "OK" : "ERR", "\n";
 }
 
 ##
@@ -46,23 +51,97 @@ BEGIN
 BEGIN { $TYPEINFO{Read} = ["function", "boolean"]; }
 sub Read
 {
-    my $ref = SCR::Read('.etc.ipsec.conf');
-
-    return Boolean(0) if(ref($ref) ne 'HASH');
-
-    %settings = %{$ref->{"config setup"}};
-
-    delete $ref->{"config setup"};
-
-    %connections = %{$ref};
-
-    return Boolean(1);
+#    my $ref = SCR::Read('.etc.ipsec.conf');
+#
+#    return Boolean(0) if(ref($ref) ne 'HASH');
+#
+#    %settings = %{$ref->{"config setup"}};
+#
+#    delete $ref->{"config setup"};
+#
+#    %connections = %{$ref};
+#
+    print STDERR "IPsecConfig::Read() FreeSwanUtils => ",
+                 $fsutil ? "OK" : "ERR", "\n";
+    %settings = ();
+    %connections = ();
+    if($fsutil and $fsutil->load_config()) {
+	# FIXME: access methods
+	#
+	# blessed  $config = {
+	#   'version' => "2.0",
+	#   'include' => [
+	#	'incl'	=> 'ipsec.*.conf',     # include glob
+	#	'file'	=> '/etc/ipsec.conf',  # included from
+	#	'list'	=> [                   # expanded glob
+	#	    '/etc/ipsec.foo.conf',
+	#           '/etc/ipsec.bar.conf',
+	#      ]
+	#   ],
+	#   'setup' => {
+	#	'nat_traversal'	=> 'yes'
+	#	'rp_filter'	=> '%unchanged'
+	#   },
+	#   'conn'      => {
+	#       '%default' => {
+	#	    'file'    => '/etc/ipsec.conf',
+	#           'data'    => {
+	#		'auto'	=> 'ignore',
+	#           }
+	#       },
+	#	'roadwarrior' => {
+	#	    'file'    => '/etc/ipsec.conf',
+	#           'data'    => {
+	#		'auto'	=> 'start',
+	#		'right' => '%defaultroute',
+	#           }
+	#	}
+	#   }
+	# };
+	#
+	print STDERR "IPsecConfig::Read() ",
+	             "FreeSwanConfig->load_config() => OK\n";
+	%settings = %{$fsutil->{'setup'}};
+	for my $name (keys %{$fsutil->{'conn'} || {}}) {
+	    next unless(exists($fsutil->{'conn'}->{$name}->{'data'}));
+	    print STDERR "connections += $name\n";
+	    $connections{$name} = $fsutil->{'conn'}->{$name}->{'data'};
+	}
+	return Boolean(1);
+    } else {
+	print STDERR "ipsec.conf parsing error: ",
+	             $fsutil->errstr(), "\n";
+    }
+    return Boolean(0);
 }
 
 BEGIN { $TYPEINFO{Connections} = ["function", [ "map", "string", [ "map", "string", "string" ]]]; }
 sub Connections()
 {
+    print STDERR "IPsecConfig::Connections() => {\n";
+    for my $name (sort keys %connections) {
+	my $conn = $connections{$name};
+	print STDERR "\tconn ", $name, " => {\n";
+	for my $key (sort keys %{$conn}) {
+	    my $val = $conn->{$key};
+	    print STDERR "\t\t$key=$val\n";
+	}
+	print STDERR "\t},\n";
+    }
+    print STDERR "}\n";
     return \%connections;
+}
+
+BEGIN { $TYPEINFO{Settings} = ["function", [ "map", "string", "string" ]]; }
+sub Settings()
+{
+    print STDERR "IPsecConfig::Settings() => {\n";
+    for my $key (sort keys %settings) {
+	my $val = $settings{$key};
+	print STDERR "\t$key=$val\n";
+    }
+    print STDERR "}\n";
+    return \%settings;
 }
 
 ##
